ac58b129c4a63db32dd793b6f436dc47b0e8ed13
Royal Choudhary <royalchoudhary74176@gmail.com>
2023-10-05 15:53:46 +0530

    adding backend

--- StoreifyFrontend/src/features/common/Footer.js
+++ StoreifyFrontend/src/features/common/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
       <div className=" bg-gray-900">
         <div className="max-w-2xl mx-auto text-white py-10">
           <div className="text-center">
-            <h3 className="text-3xl mb-3"> Download our Ecommerce App </h3>
+            <h3 className="text-3xl mb-3"> Download our Storiefy App </h3>
             <p> Buy what you want. </p>
             <div className="flex justify-center my-10">
               <div className="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2">
@@ -31,8 +31,8 @@ function Footer() {
           </div>
           <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
             <p className="order-2 md:order-1 mt-8 md:mt-0">
-              {' '}
-              © CoderDost, 2023.{' '}
+              {" "}
+              © Royal Choudhary, 2023.{" "}
             </p>
             <div className="order-1 md:order-2">
               <span className="px-2">About us</span>
